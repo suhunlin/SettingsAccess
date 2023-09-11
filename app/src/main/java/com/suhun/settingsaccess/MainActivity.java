3aa149db3ef62aab628a292db459aa8da6d1fcb4
suhunlin <lin.suhun@msa.hinet.net>
2023-09-11 10:05:25 +0800

    Add ListView and initial ListView

--- app/src/main/java/com/suhun/settingsaccess/MainActivity.java
+++ app/src/main/java/com/suhun/settingsaccess/MainActivity.java
@@ -7,24 +7,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private String tag = MainActivity.class.getSimpleName();
     private TextView fieldName;
     private ListView showContentValues;
     private Button getFieldName, getContentValues;
+    private SimpleAdapter simpleAdapter;
+    private ArrayList<HashMap<String, String>> data = new ArrayList<>();
+    private String[] from = {"layout_item"};
+    private int[] to = {R.id.content_item};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initListView();
     }
 
     private void initView(){
         fieldName = findViewById(R.id.lid_fieldName);
-        showContentValues = findViewById(R.id.lid_contentValues);
         getFieldName = findViewById(R.id.lid_getFieldName);
         getContentValues = findViewById(R.id.lid_getSetingContent);
         getFieldName.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +50,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initListView(){
+        showContentValues = findViewById(R.id.lid_contentValues);
+        simpleAdapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+        showContentValues.setAdapter(simpleAdapter);
+    }
+
 
 }
